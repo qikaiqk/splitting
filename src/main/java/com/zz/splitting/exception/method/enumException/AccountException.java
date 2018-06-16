513936dbc8cad1a2f6d796c8cea4e54cfe9356e9
qikaiqk <qikaiqk@gmail.com>
2018-06-16 01:11:43 -0400

    notes update

---
+++ src/main/java/com/zz/splitting/exception/method/enumException/AccountException.java
@@ -0,0 +1,28 @@
+package com.zz.splitting.exception.method.enumException;
+
+public class AccountException extends Exception {
+
+    private Integer code;
+    private String msg;
+
+    AccountException(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
---
+++ src/main/java/com/zz/splitting/exception/method/enumException/Drive.java
@@ -0,0 +1,8 @@
+package com.zz.splitting.exception.method.enumException;
+
+public class Drive {
+
+    public static void main(String[] args) {
+        System.out.println(AccountExceptionEnum.ACCOUNT_ALREADY_EXIST.getMsg());
+    }
+}
