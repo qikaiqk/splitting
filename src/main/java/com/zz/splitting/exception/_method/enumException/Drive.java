8cf8522274ab2597cbed0b2130e5b3114d532787
qikaiqk <qikaiqk@gmail.com>
2018-07-16 09:58:15 +0800

    .gitignore

--- src/main/java/com/zz/splitting/exception/_method/enumException/Drive.java
+++
@@ -1,8 +0,0 @@
-package com.zz.splitting.exception._method.enumException;
-
-public class Drive {
-
-    public static void main(String[] args) {
-        System.out.println(AccountExceptionEnum.ACCOUNT_ALREADY_EXIST.getMsg());
-    }
-}
